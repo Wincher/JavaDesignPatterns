b60b3ebff7601ad6508d1714834af14040105509
Wincher <wenqiangshanghai@163.com>
2017-03-07 10:25:06 +0800

    update

--- src/com/wincher/proxy/myDynamicProxy/Client.java
+++ src/com/wincher/proxy/myDynamicProxy/Client.java
@@ -3,21 +3,15 @@ package com.wincher.proxy.myDynamicProxy;
 import java.io.IOException;
 
 public class Client {
-	
-	
+
 	public static void main(String[] args) throws Exception {
 		Tank t = new Tank();
 		TimeInvocationHandler h = new TimeInvocationHandler(t);
-		try {
-			//Moveable m = (Moveable) Proxy.newProxyInstance(Moveable.class,h);
-			Moveable m = (Moveable) h.getProxy(Moveable.class);
-			m.move();
-			m.fly();
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+		Moveable m = (Moveable) h.getProxy(Moveable.class);
+		m.move();
+		m.fly();
 	}
-	
+
 	public static void test1(String[] args) {
 		Moveable m = new TankTimeProxy(new Tank());
 		Moveable m2 = new TankLogProxy(m);
