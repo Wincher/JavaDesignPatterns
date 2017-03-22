430325d8d2ba81928e2473465ef807a8f02e8921
Wincher <wincher.hoo@gmail.com>
2017-03-22 10:57:46 +0800

    add facade

---
+++ src/com/wincher/facade/Client.java
@@ -0,0 +1,14 @@
+package com.wincher.facade;
+
+/**
+ * Created by Wincher on 2017/3/22.
+ */
+public class Client {
+    public static void main(String[] args) {
+        ModuleFacade mf = new ModuleFacade();
+
+        mf.a1();
+        mf.b1();
+        mf.c1();
+    }
+}
